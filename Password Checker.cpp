4b9f5b38ee8a5f629b38b6b1855fa3b2184ef327
SantaKep <61010440+SantaKep@users.noreply.github.com>
2020-12-29 16:25:21 +0200

    Add files via upload

---
+++ Password Checker.cpp
@@ -0,0 +1,30 @@
+#include "Header.h"
+
+using namespace std;
+
+int main()
+{
+	int code, code2;
+	cout << "Welcome " << endl;
+	cout << "Enter your code " << endl;
+	cin >> code;
+	cout << "Your code is " << code << endl;
+	if (code == 5670750)
+	{
+		cout << "Password is already used" << endl;
+	}
+	else
+	{
+		cout << "Confirm your password" << endl;
+		cin >> code2;
+		if (code == code2)
+		{
+			cout << "Password accepted" << endl;
+		}
+		else
+		{
+			cout << "Wrong password 1 and password 2" << endl;
+		}
+	}
+	return 0;
+}
